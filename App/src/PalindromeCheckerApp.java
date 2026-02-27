9284056c1927060eb8b1a02a5203ca0b342dfd89
stutikashyap <stutikashyap005@gmail.com>
2026-02-27 09:26:39 +0530

    Stuti Kashyap added UC -6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -8,11 +8,13 @@ public class PalindromeCheckerApp {
         for(char ch : text.toCharArray()) {
             stack.push(ch);
         }
+        Queue<Character> queue = new LinkedList<>();
+        for(char ch : text.toCharArray()) {
+            queue.offer(ch);
+        }
         boolean isPalindrome = true;
-        char ch;
-        for(int i = 0; i < text.length(); i++){
-            ch = stack.pop();
-            if(ch != text.charAt(i)){
+        while(!queue.isEmpty()) {
+            if(queue.poll() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
