df8afbf41cd6d85284ae3b4fda856194d64391c7
stutikashyap <stutikashyap005@gmail.com>
2026-02-27 09:02:33 +0530

    Stuti Kashyap added UC -4

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,15 +1,17 @@
-public class UseCase3PalindromeCheckerApp {
-
-   import java.util.*;
-    public class PalindromeCheckerApp {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Input text: ");
-            String text = sc.nextLine();
-            String reverse = "";
-            for(int i = text.length() - 1; i >= 0; i--) {
-                reverse += text.charAt(i);
+import java.util.*;
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String text = sc.nextLine();
+        char[] arr = text.toCharArray();
+        boolean isPalindrome = true;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != arr[arr.length - i - 1]){
+                isPalindrome = false;
+                break;
             }
-            System.out.println("Is it a Palindrome? : " + text.equals(reverse));
         }
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
+}
